77c90d7b85424c650825239e40e470c042bca3f0
ewier <e.wierciak@gmail.com>
2023-04-17 13:18:32 +0200

    temp

--- projekt/sphincs.py
+++ projekt/sphincs.py
@@ -3,8 +3,7 @@ SPHINCS+ implementation
 
 '''
 from utils import *
-from math import floor
-
+from math import floor, ceil, log
 
 class Sphincs():
     '''
@@ -17,11 +16,16 @@ class Sphincs():
 
     m: the message digest length in bytes
     len: the number of n-byte string elements in a WOTS+ private key, public key, and signature.
+
+    VALUES:
+
+    n: it is the message length as well as the length of a private key, public key, or signature element in bytes.
+    w: it is an element of the set {4, 16, 256}
     
     '''
 
     def __init__(self, n, w, h, d, k, t, randomise = True):
-        self.n = n
+        self.n = n # 
         self.w = w
         self.h = h
         self.d = d
@@ -41,85 +45,85 @@ class Sphincs():
         return ( (SK_seed, SK_prf, PK_seed, PK_root), (PK_seed, PK_root) )
 
 
-    def spx_sign(self, M, SK):
-        '''
-        returns a SPHINCS+ signature SIG
+    # def spx_sign(self, M, SK):
+    #     '''
+    #     returns a SPHINCS+ signature SIG
 
-        M : message 
-        SK : private key, SK = (SK.seed, SK.prf, PK.seed, PK.root)
+    #     M : message 
+    #     SK : private key, SK = (SK.seed, SK.prf, PK.seed, PK.root)
 
-        '''
-        SK_seed, SK_prf, PK_seed, PK_root = SK
+    #     '''
+    #     SK_seed, SK_prf, PK_seed, PK_root = SK
 
-        # init
-        ADRS = self.toByte(0, 32)
-        # generate randomizer
-        opt = self.toByte(0, self.n)
-        if self.RANDOMIZE:
-            opt = self.rand(self.n)
-        R = self.RF_msg(SK_prf, opt, M)
-        SIG = SIG || R
+    #     # init
+    #     ADRS = self.toByte(0, 32)
+    #     # generate randomizer
+    #     opt = self.toByte(0, self.n)
+    #     if self.RANDOMIZE:
+    #         opt = self.rand(self.n)
+    #     R = self.RF_msg(SK_prf, opt, M)
+    #     SIG = SIG || R
 
-        # compute message digest and index
-        digest = H_msg(R, PK_seed, PK_root, M)
-        lengths = [floor((ka +7)/ 8), floor((h - h/d +7)/ 8), floor((h/d +7)/ 8)]
-        tmp_md, tmp_idx_tree, tmp_idx_leaf = extract_bytes(digest, lengths)
-        md = md[:ka] # first ka bits of tmp_md
-        idx_tree = tmp_idx_tree[:(h - h/d)] # first h - h/d bits of tmp_idx_tree
-        idx_leaf = tmp_idx_leaf[:(h/d)] # first h/d bits of tmp_idx_leaf
+    #     # compute message digest and index
+    #     digest = H_msg(R, PK_seed, PK_root, M)
+    #     lengths = [floor((ka +7)/ 8), floor((h - h/d +7)/ 8), floor((h/d +7)/ 8)]
+    #     tmp_md, tmp_idx_tree, tmp_idx_leaf = extract_bytes(digest, lengths)
+    #     md = md[:ka] # first ka bits of tmp_md
+    #     idx_tree = tmp_idx_tree[:(h - h/d)] # first h - h/d bits of tmp_idx_tree
+    #     idx_leaf = tmp_idx_leaf[:(h/d)] # first h/d bits of tmp_idx_leaf
         
-        # FORS sign
-        ADRS.setLayerAddress(0)
-        ADRS.setTreeAddress(idx_tree)
-        ADRS.setType(FORS_TREE)
-        ADRS.setKeyPairAddress(idx_leaf)
-        SIG_FORS = fors_sign(md, SK.seed, PK_seed, ADRS)
-        SIG = SIG || SIG_FORS
+    #     # FORS sign
+    #     ADRS.setLayerAddress(0)
+    #     ADRS.setTreeAddress(idx_tree)
+    #     ADRS.setType(FORS_TREE)
+    #     ADRS.setKeyPairAddress(idx_leaf)
+    #     SIG_FORS = fors_sign(md, SK.seed, PK_seed, ADRS)
+    #     SIG = SIG || SIG_FORS
         
-        # get FORS public key
-        PK_FORS = fors_pkFromSig(SIG_FORS, M, PK_seed, ADRS)
+    #     # get FORS public key
+    #     PK_FORS = fors_pkFromSig(SIG_FORS, M, PK_seed, ADRS)
         
-        # sign FORS public key with HT
-        ADRS.setType(TREE)
-        SIG_HT = ht_sign(PK_FORS, SK_seed, PK_seed, idx_tree, idx_leaf)
-        SIG = SIG || SIG_HT
-        return SIG
+    #     # sign FORS public key with HT
+    #     ADRS.setType(TREE)
+    #     SIG_HT = ht_sign(PK_FORS, SK_seed, PK_seed, idx_tree, idx_leaf)
+    #     SIG = SIG || SIG_HT
+    #     return SIG
 
 
-    def spx_verify(self, M, SIG, PK):
-        '''
-        returns boolean value that denotes the verification of the given signature
+    # def spx_verify(self, M, SIG, PK):
+    #     '''
+    #     returns boolean value that denotes the verification of the given signature
 
-        M : message 
-        SIG : signature
-        PK : public key 
-        '''
-        PK_seed, PK_root = PK
+    #     M : message 
+    #     SIG : signature
+    #     PK : public key 
+    #     '''
+    #     PK_seed, PK_root = PK
 
-        # init
-        ADRS = toByte(0, 32);
-        R = SIG.getR();
-        SIG_FORS = SIG.getSIG_FORS();
-        SIG_HT = SIG.getSIG_HT();
+    #     # init
+    #     ADRS = toByte(0, 32);
+    #     R = SIG.getR();
+    #     SIG_FORS = SIG.getSIG_FORS();
+    #     SIG_HT = SIG.getSIG_HT();
         
-        # compute message digest and index
-        digest = H_msg(R, PK_seed, PK_root, M);
-        lengths = [floor((ka +7)/ 8), floor((h - h/d +7)/ 8), floor((h/d +7)/ 8)]
-        tmp_md, tmp_idx_tree, tmp_idx_leaf = extract_bytes(digest, lengths)
-        md = md[:ka] # first ka bits of tmp_md
-        idx_tree = tmp_idx_tree[:(h - h/d)] # first h - h/d bits of tmp_idx_tree
-        idx_leaf = tmp_idx_leaf[:(h/d)] # first h/d bits of tmp_idx_leaf
+    #     # compute message digest and index
+    #     digest = H_msg(R, PK_seed, PK_root, M);
+    #     lengths = [floor((ka +7)/ 8), floor((h - h/d +7)/ 8), floor((h/d +7)/ 8)]
+    #     tmp_md, tmp_idx_tree, tmp_idx_leaf = extract_bytes(digest, lengths)
+    #     md = md[:ka] # first ka bits of tmp_md
+    #     idx_tree = tmp_idx_tree[:(h - h/d)] # first h - h/d bits of tmp_idx_tree
+    #     idx_leaf = tmp_idx_leaf[:(h/d)] # first h/d bits of tmp_idx_leaf
 
-        # compute FORS public key
-        ADRS.setLayerAddress(0);
-        ADRS.setTreeAddress(idx_tree);
-        ADRS.setType(FORS_TREE);
-        ADRS.setKeyPairAddress(idx_leaf);
-        PK_FORS = fors_pkFromSig(SIG_FORS, md, PK_seed, ADRS);
+    #     # compute FORS public key
+    #     ADRS.setLayerAddress(0);
+    #     ADRS.setTreeAddress(idx_tree);
+    #     ADRS.setType(FORS_TREE);
+    #     ADRS.setKeyPairAddress(idx_leaf);
+    #     PK_FORS = fors_pkFromSig(SIG_FORS, md, PK_seed, ADRS);
         
-        # verify HT signature
-        ADRS.setType(TREE);
-        return ht_verify(PK_FORS, SIG_HT, PK_seed, idx_tree, idx_leaf, PK_root);
+    #     # verify HT signature
+    #     ADRS.setType(TREE);
+    #     return ht_verify(PK_FORS, SIG_HT, PK_seed, idx_tree, idx_leaf, PK_root);
 
 
 
